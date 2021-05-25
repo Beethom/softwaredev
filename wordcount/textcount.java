33e83240afc31e2f972e2cc6a1f8dba3d172b25e
Beethoven <bmarhone@mail.valenciacollege.edu>
2021-05-25 18:01:56 -0400

    Fix project

---
+++ wordcount/textcount.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+//app for word count
+public class textcount{
+    public static void main (String [] args) throws FileNotFoundException {
+        //Declare collection for returning pair of words and numbers, the collection name is words
+        HashMap <String, Integer>words = new HashMap<String, Integer>();
+        String fileName = "src\\poem.txt";
+        File textFile = new File(fileName);
+        Scanner in = new Scanner(textFile);
+       // File in2 = new File("src\\poem.txt");
+        //Scanner in = new Scanner(in2);
+        //loop to repeat condition while reading the file.
+        while (in.hasNext()) {
+            String ocu = in.next();
+
+            ocu=ocu.replaceAll("[^a-zA-Z]","");
+            ocu.toLowerCase();
+            if (words.containsKey(ocu)) {
+                int wordcount = words.get(ocu);
+                words.remove(ocu);
+                words.put(ocu, wordcount+1);
+            }
+            else {
+                words.put(ocu, 1);
+            }
+        }
+        in.close();
+        Set<Map.Entry<String, Integer>> Hash_set = words.entrySet();
+        List<Map.Entry<String, Integer>> wordSorted = new ArrayList<Map.Entry<String, Integer>>(Hash_set);
+        Collections.sort( wordSorted, new Comparator<Map.Entry<String, Integer>>()
+        {
+            public int compare( Map.Entry<String, Integer> x, Map.Entry<String, Integer> y )
+
+            {
+                // sortList in descending order
+                return ( y.getValue()).compareTo( x.getValue() );
+
+            }
+
+        } );
+            for (Map.Entry<String, Integer > entry: wordSorted)
+            System.out.println(entry);
+
+
+        }
+    }
+
+
